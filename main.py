561b8b87aac4eed8a033e17833ad899f44bfa0ec
egwolk <erincovacha@gmail.com>
2026-02-23 19:53:08 +0800

    Enhance file reading and writing functionality with user prompts and input handling

--- main.py
+++ main.py
@@ -3,17 +3,45 @@ import os, sys
 def read_file(filename):
     try:
         with open(filename, "r") as file:
-            content = file.read()
-            print(content)
+            print(f"{filename} contents:")
+            for line in file:
+                print(f"{line.strip()}")
+            input("Press enter key to continue...")
     except:
         print("file does not exist")
+        input("Press enter key to continue...")
 
 def input_file_name():
-    file = input("Enter file name: ")
+    file = input("Enter txt file name[no extension]: ")
     return file + ".txt"
 
-def write_file():
-    pass
+def write_file(filename):
+    if os.path.exists(filename):
+        print(f"File with name {filename} already exists.\nOverwrite file?")
+        while True:
+            yn = input("Y|N: ").lower().strip()
+            match yn:
+                case "y":
+                    content = []
+                    clear_console()
+                    print("Start writing your file content [press enter twice to finish]:")
+                    while True:
+                        line = input()
+                        if line:
+                            content.append(line + "\n")
+                        else: break
+                    with open(filename, "w") as file:
+                        file.writelines(content)
+                    break
+                case "n":
+                    print("operation cancelled")
+                    input("Press enter key to continue...")
+                    break
+                case _:
+                    print("invalid input. Try again")
+            
+    
+
 def append_file():
     pass
 def rename_file():
@@ -21,34 +49,48 @@ def rename_file():
 def delete_file():
     pass
 
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-print("""
+while True:
+    clear_console()
+    print("""
 What would you like to do?
 [1] Read a file
 [2] Create a new file
 [3] Add data to a file
 [4] Rename a file 
 [5] Delete a file
-[6] Exit
+[6] Read program docs
+[7] Exit
 """)
-while True:
-    user_input = input("Your choice: ")
-    match user_input:
-        case "1":
-            file = input_file_name()
-            read_file(file)
-            break
-        case "2":
-            break
-        case "3":
-            break
-        case "4":
-            break
-        case "5":
-            break
-        case "6":
-            sys.exit("bye bye")
-        case _:
-            print("invalid input. try again")
-
-print(f"You picked {user_input}")
+    while True:
+        user_input = input("Your choice: ")
+        match user_input:
+            case "1":
+                clear_console()
+                file = input_file_name()
+                read_file(file)
+                break
+            case "2":
+                clear_console()
+                file = input_file_name()
+                write_file(file)
+                break
+            case "3":
+                clear_console()
+                break
+            case "4":
+                clear_console()
+                break
+            case "5":
+                clear_console()
+                break
+            case "6":
+                clear_console()
+                break
+            case "7":
+                clear_console()
+                sys.exit("bye bye")
+            case _:
+                print("invalid input. try again")
